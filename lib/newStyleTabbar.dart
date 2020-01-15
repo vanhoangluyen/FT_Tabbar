bc10167fbbb57bad8e3da11becb88156ff52e3cb
Luyenhv <luyenhv@beetsoft.com.vn>
2020-01-15 09:05:09 +0700

    [UD] update new style

--- lib/newStyleTabbar.dart
+++ lib/newStyleTabbar.dart
@@ -58,3 +58,60 @@ Widget bottomTabbar() {
     ),
   );
 }
+
+class Bookkeeper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color(0xFF3F5AA6),
+            title: Text("Title text"),
+          ),
+          bottomNavigationBar: menu(),
+          body: TabBarView(
+            children: [
+              Container(child: Icon(Icons.directions_car)),
+              Container(child: Icon(Icons.directions_transit)),
+              Container(child: Icon(Icons.directions_bike)),
+              Container(child: Icon(Icons.directions_bike)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget menu() {
+    return Container(
+      color: Color(0xFF3F5AA6),
+      child: TabBar(
+        labelColor: Colors.white,
+        unselectedLabelColor: Colors.white70,
+        indicatorSize: TabBarIndicatorSize.tab,
+        indicatorPadding: EdgeInsets.all(5.0),
+        indicatorColor: Colors.blue,
+        tabs: [
+          Tab(
+            text: "Transactions",
+            icon: Icon(Icons.euro_symbol),
+          ),
+          Tab(
+            text: "Bills",
+            icon: Icon(Icons.assignment),
+          ),
+          Tab(
+            text: "Balance",
+            icon: Icon(Icons.account_balance_wallet),
+          ),
+          Tab(
+            text: "Options",
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
+    );
+  }
+}
